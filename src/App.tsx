f05201aecc2eae43243cab27e5211d500e4a6fc3
Pallab Hazra <hazrap404@gmail.com>
2024-12-26 20:37:53 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import React from "react";
+import {BrowserRouter, Route,Routes} from "react-router-dom";
+import Home from "./Pages/Home.tsx";
+import Auth from "./Pages/Auth";
+function App(){
+    return(
+        <BrowserRouter>
+            <div className={'flex min-h-screen w-full bg-[#FAFAFA]'}>
+                <Routes>
+                <Route path={'/'} Component={Auth} />
+                <Route path={'/dashboard'} Component={Home} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
+}
+export default App;
